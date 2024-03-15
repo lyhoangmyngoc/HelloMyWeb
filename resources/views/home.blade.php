56b4487366e81f309684b3a2444a37b363c23478
lyhoangmyngoc <myn63251@gmail.com>
2024-03-16 00:24:04 +0700

    HelloMyWeb

---
+++ resources/views/home.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Intership</title>
+        <link rel="icon" type="image/x-icon" href="client/img/th.jpg" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="server/css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color: rgba(0, 0, 0, 0.7)">
+            <div class="container px-10">
+                <a class="navbar-brand" style="color: coral;
+                font-size: 35px;
+                position: absolute; padding-left: 260px" href="#">edupath</a>
+            </div>
+            <div class="container px-5" style="margin-left: 70px">
+                <span class="navbar-brand" style="font-size: 18px">Test Prep & College Explorer</span>
+            </div>
+            <div class="container px-5" style="margin: 0 0 -18px 0;">   
+                <button style=" background-color: coral; margin: 10px 0 0 0; border-radius: 7px; height: 26px " type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span style="color: #fff; font-size: 9px; display: flex; align-items: center">Send your Teen the App</span>
+                </button>
+            </div>
+        </nav>
+        <!-- Header-->
+        
+        <header class="masthead text-center text-white">
+            <div class="container-sm">
+                <div style="padding-bottom: 35px">
+                    <h2 style="color: black; font-size: 50px">Hello My WebSite</h2>
+                    <h5 style="color: black; font-size: 30px">Lý Hoàng Mỹ Ngọc</h5>
+                </div>
+                <div style="background-color: #fff; padding: 2%">
+                    <div>
+                        <h5 style="padding-right: 25rem ;color: coral">Sent the app to your student now</h5>
+                    </div>
+                    <div class="row g-3">
+                        <div class="col-sm-2">
+                            <input type="text" class="form-control" placeholder="your name" name="name">
+                        </div>
+                        <div class="col-sm-2">
+                            <input type="text" class="form-control" placeholder="student's email" name="email">
+                        </div>
+                        <div class="col-sm-2">
+                            <input type="text" class="form-control" placeholder="student's mobile" name="mobile">
+                        </div>
+                        <div class="col-sm-2">
+                            <input type="text" class="form-control" placeholder="SAT" name="sat">
+                        </div>
+                        <div class="col-sm-1">
+                            <button type="submit" class="btn btn-info">Sent</button>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section id="scroll">
+            <div style="background-color: bisque">
+                <div style="padding-top: 3%">
+                    <h5 style="color: black; padding: 20px 0px 20px 540px; font-size: 40px">Edupath</h5>
+                </div>
+                <div class="row" style="padding-top: 3%">
+                    <div class="col-sm-5">
+                        <p>- Thành lập năm 2010 bởi: CÔNG TY TNHH CON ĐƯỜNG GIÁO DỤC.<br>
+                            - EduPath khẳng định bằng cam kết luôn sát cánh cùng các bạn du học sinh trên con đường chinh phục tri thức mới, vượt qua mọi rào cản và đặc biệt là chiến thắng bản thân để vươn tới thành công.<br>
+                            - EduPath đã cùng hàng ngàn học sinh chạm ngõ những trường Đại học danh giá tại các quốc gia phát triển như Mỹ, Canada, Anh, Úc, New Zealand, Singapore, Philippines và Châu Âu. Trong đó có vô số các bạn đã giành được những suất học bổng danh giá 50 – 100%.<br> 
+                            - Không dừng lại ở đó, EduPath còn giúp đỡ các du học sinh tìm kiếm cơ hội việc làm sau khi tốt nghiệp, hỗ trợ các gia đình Việt có mong muốn định cư nước ngoài theo những chương trình làm việc và đầu tư.<br>
+                            - EduPath tràn đầy hy vọng và tin tưởng rằng, trên bước đường thành công của các em học sinh, trong giấc mơ định cư của các gia đình Việt trong tương lai sẽ mang đậm dấu ấn của EduPath.</p>
+                    </div>
+                    <div class="col-sm-5">
+                        <img  style="height: 65%" src="server/assets/img/duhoc.jpg">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="scroll">
+            <div style="background-color: silver">
+                <div>
+                    <h5 style="color: black; padding: 20px 0px 20px 460px;">Học trực tuyến tại trung tập ABC</h5>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <p><i class="fas fa-check"></i>Học trực tuyến cho phép bạn học một khóa học đáp ứng nhu cầu của bạn. Có các khóa học dành cho những người có khả năng ngôn ngữ khác nhau.</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <p><i class="fas fa-check"></i>Khi đăng ký với chúng tôi, bạn sẽ làm một bài kiểm tra giúp chúng tôi hiểu nhu cầu của bạn và xếp bạn vào lớp học tốt nhất có thể. Chúng tôi cũng sẽ đưa ra khuyến nghị về thời gian bạn cần học.</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <p><i class="fas fa-check"></i>Điều này dựa trên kế hoạch học tập của bạn. Ví dụ, hầu hết các trường trung học đều yêu cầu trình độ tiếng Anh trên trung cấp. Nếu điểm trong bài kiểm tra của bạn cho thấy bạn ở trình độ tiền trung cấp thì bạn có thể phải học từ 24 đến 30 tuần để đạt đến trình độ trung cấp cao hơn.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {{-- <!-- Content section 1-->
+        
+        <!-- Content section 2-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="server/assets/img/02.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2 class="display-4">We salute you!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 3-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="server/assets/img/03.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> --}}
+        <!-- Footer-->
+        <footer style="background-color: coral">
+            <div class="container px-5"><p class="m-0 text-center text-white small">MyNgoc's Website 2024</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="server/js/scripts.js"></script>
+    </body>
+</html>
